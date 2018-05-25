711a01b1d030498783ffa027a4bc487c2e4d7477
Cristiano Balducci <cristiano.balducci@gmail.com>
2018-05-25 11:13:22 +0100

    [INFRA-002] Add view cumulative_sale_last_year

---
+++ infrastructure/DDLs/cumulative_sale_last_year.sql
@@ -0,0 +1,7 @@
+-- object: ottoma.cumulative_sale_last_year | type: VIEW --
+-- DROP VIEW ottoman.cumulative_sale_last_year;
+CREATE VIEW ottoman.cumulative_sale_last_year
+ AS SELECT *
+ FROM ottoman.cumulative_sale
+ WHERE local_date > CURRENT_DATE - INTERVAL '1 year';
+-- ddl-end --
