893128f94cc6dce4ae9d2f832644587bd1bcebfe
Cristiano Balducci <cristiano.balducci@gmail.com>
2018-05-25 12:02:56 +0100

    [INFRA-002] Change on_promotion to boolean

--- infrastructure/DDLs/transaction_content.sql
+++ infrastructure/DDLs/transaction_content.sql
@@ -4,7 +4,7 @@ CREATE TABLE ottoman.transaction_content(
 	transaction_id bigint NOT NULL,
   item_nbr bigint,
 	unit_sale smallint,
-	on_promotion smallint,
+	on_promotion boolean,
 	CONSTRAINT pk_transaction_content_id_item_nbr PRIMARY KEY (transaction_id, item_nbr)
 );
 -- ddl-end --
