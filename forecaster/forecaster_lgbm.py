46f1af2e06bace88607cf660a5ba1e1be35986a4
Bryan Feeney <bryan.feeney@ncr.com>
2018-05-25 08:41:37 +0100

    Got machine learning working in a script

--- forecaster/forecaster_lgbm.py
+++ forecaster/forecaster_lgbm.py
@@ -133,11 +133,58 @@ def create_machine_learning_matrices(cumul_sales, promos, start_date, current_da
 
     return X_train, y_train, X_val, y_val, X_test
 
+def train_model(items, X_train, y_train, X_val, y_val, X_test, params=None, maxRounds=5000):
+    if params is None:
+        params = {
+            'num_leaves': 2**5 - 1,
+            'objective': 'regression_l2',
+            'max_depth': 8,
+            'min_data_in_leaf': 50,
+            'learning_rate': 0.05,
+            'feature_fraction': 0.75,
+            'bagging_fraction': 0.75,
+            'bagging_freq': 1,
+            'metric': 'l2',
+            'num_threads': 4
+        }
+    val_pred = []
+    test_pred = []
+    cate_vars = []
+    for i in range(FutureDaysToCalculate):
+        print("=" * 50)
+        print("Step %d" % (i+1))
+        print("=" * 50)
+        dtrain = lgb.Dataset(
+            X_train, label=y_train[:, i],
+            categorical_feature=cate_vars,
+            weight=pd.concat([items["perishable"]] * 4) * 0.25 + 1
+        )
+        dval = lgb.Dataset(
+            X_val, label=y_val[:, i], reference=dtrain,
+            weight=items["perishable"] * 0.25 + 1,
+            categorical_feature=cate_vars)
+        bst = lgb.train(
+            params, dtrain, num_boost_round=maxRounds,
+            valid_sets=[dtrain, dval], early_stopping_rounds=50, verbose_eval=50
+        )
+        print("\n".join(("%s: %.2f" % x) for x in sorted(
+            zip(X_train.columns, bst.feature_importance("gain")),
+            key=lambda x: x[1], reverse=True
+        )))
+        val_pred.append(bst.predict(
+            X_val, num_iteration=bst.best_iteration or maxRounds))
+        test_pred.append(bst.predict(
+            X_test, num_iteration=bst.best_iteration or maxRounds))
+
+    rmse = np.sqrt (mean_squared_error(np.expm1(y_val), np.expm1(np.array(val_pred)).transpose()))
+
+    return val_pred, test_pred, rmse
+
 
 if __name__ == "__main__":
     (loc, min_date, cumul_sale_tbl, cumul_sale_future_tbl, items_tbl, stores_tbl) = sys.argv[1:]
     if loc == "csv":
-        cumul_sales, cumul_sales_future, stores, items = \
+        cumul_sales, cumul_sales_future, items, stores = \
             load_data_csv(cumul_sale_tbl, cumul_sale_future_tbl, items_tbl, stores_tbl)
     elif loc == "sql":
         raise ValueError ("Not implemented")
@@ -160,10 +207,13 @@ if __name__ == "__main__":
     # How far back to go to start generating trend features for demand
     history_start = now - timedelta(7*WeeksOfHistoryForFeature) + timedelta(1)
     test_start    = now - timedelta(7*WeeksOfHistoryForFeatureOnTest) + timedelta(1)
-    create_machine_learning_matrices(\
+    X_train, y_train, X_val, y_val, X_test = create_machine_learning_matrices(\
         cumul_sales, promo_variables, \
         start_date=history_start, current_date=now, test_start_date=test_start)
 
+    # Train a separate model for each of the next `FutureDaysToCalculate`
+    val_pred, test_pred, rmse = train_model(items, X_train, y_train, X_val, y_val, X_test)
+    print ("Validation error is :" + str(rmse))
 
 
 
